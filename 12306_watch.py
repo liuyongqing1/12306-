574a30213e4e74d3a8a313042a94e08e2c768409
liuyongqing1 <lyq19910219@hotmali.com>
2018-01-22 17:46:56 +0800

    添加代码

---
+++ 12306_watch.py
@@ -0,0 +1,48 @@
+#-*- coding:utf8 -*-
+
+from pyquery import PyQuery as pq
+import requests,time
+
+
+class fuck12306(object):
+    def __init__(self):
+        self.url='http://www.12306.cn/mormhweb/1/4/index_fl.html'
+        self.r=requests.session()
+        self.r.headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+                        'Referer':'http://www.12306.cn/mormhweb/1/4/index_fl.html'}
+        self.one=[]
+
+    def req_html(self):
+        html=self.r.get(self.url)
+        html.encoding='utf-8'
+        return html.text
+
+    def res_html_one(self):
+        html=self.req_html()
+        doc=pq(html)
+        ulli=doc.find('#newList ul li')
+        for i in ulli.items():
+            item=i.text()
+            self.one.append(item)
+        return self.one
+
+    def xh(self):
+        count=0
+        html=self.req_html()
+        doc=pq(html)
+        ulli=doc.find('#newList ul li')
+        for i in ulli.items():
+            item=i.text()
+            if item not in self.one:
+                if count == 5:
+                    self.one.append(item)
+
+        return self.one
+
+if __name__ == '__main__':
+    fuck=fuck12306()
+    print(fuck.res_html_one())
+    while True:
+        time.sleep(30)
+        b=fuck.xh()
+        print(b)
